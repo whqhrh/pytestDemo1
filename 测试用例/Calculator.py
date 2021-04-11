d72348fbdb3d8854dbc378385130556303f6b4ea
whqhrh <51356920+whqhrh@users.noreply.github.com>
2021-04-11 23:17:23 +0800

    Update Calculator.py

---
+++ 测试用例/Calculator.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def sum(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
